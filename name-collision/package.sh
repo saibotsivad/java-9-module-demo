2027fc46e95de2e38c44480e58e5082f2796aad7
Tobias Davis <accounts@davistobias.com>
2016-07-23 10:13:38 -0600

    Correct the package naming.

--- name-collision/package.sh
+++ name-collision/package.sh
@@ -2,7 +2,7 @@
 
 mkdir -p mlib
 
-jar --create --file=mlib/client.jar --main-class=com.tobiaslabs.client.Client -C mods/a .
+jar --create --file=mlib/a.jar --main-class=com.tobiaslabs.client.Client -C mods/a .
 
 jar --create --file=mlib/b.jar -C mods/b .
 jar --create --file=mlib/c.jar -C mods/c .
